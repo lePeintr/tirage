9b20a390e7da6a925cc5fd3a8acae243f47401a2
ericJoel9 <fodjotchuente@gmail.com>
2022-07-23 06:53:13 +0200

    initialisation du projet

---
+++ src/main/java/com/tirage/tirage/TirageApplication.java
@@ -0,0 +1,13 @@
+package com.tirage.tirage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TirageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TirageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tirage/tirage/TirageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tirage.tirage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TirageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
